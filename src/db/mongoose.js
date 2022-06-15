0644115dfcc157ace08480d52aee9740f3c75087
DavidBolaji <odavidbolaji14@gmail.com>
2022-06-15 16:30:17 +0100

    repeating previous commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
-let server;
-if (process.env.ENV === "local") {
-    server = process.env.MONGODB_URI_LOCAL
-} else {
+let server = process.env.MONGODB_URI_LOCAL
+if (process.env.ENV === "prod") {
     server = process.env.MONGODB_URI_PROD
-}
+} 
+
 mongoose.connect(server, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
